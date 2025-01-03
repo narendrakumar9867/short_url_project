df8e09b0fb91685e13895968fea93742a6fbcc49
mrnarendra9867 <narendrakumark750@gmail.com>
2025-01-03 14:23:44 +0530

    Modified readme

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRoute = require('./routes/staticRouter');
 const URL = require("./models/url");
 const app = express();
 const PORT = 9867;
@@ -8,11 +10,23 @@ const PORT = 9867;
 connectToMongoDB("mongodb://127.0.0.1:27017/short_url").then(() => console.log('MongoDB connecteed.')
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.get("/test", async (req, res) => {
+//     const allUrls = await URL.find({});
+//     return res.render('home', {
+//         urls: allUrls,
+//     });
+// });
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
-app.get('/:shortId', async (req, res) => {
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
