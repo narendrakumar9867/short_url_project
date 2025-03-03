66feef6363710a8afe1d057f8d0fbd8b8f1873c3
mrnarendra9867 <narendrakumark750@gmail.com>
2025-03-03 22:22:07 +0530

    modified

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,9 +17,7 @@ function checkForAuthentication(req, res, next) {
 function restrictTo(roles = []) {
     return function (req, res, next) {
         if (!req.user) return res.redirect("/login");
-
         if (!roles.includes(req.user.role)) return res.end("UnAuthorized");
-
         return next();
     };
 }
